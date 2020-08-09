5ef62634dfc7ebfa1a2b4baa3d6e299d4f24ff6b
Jackseed <vinkho.c@gmail.com>
2020-08-09 13:17:07 +0200

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ReceiptCalculationComponent } from './receipt-calculation/receipt-calculation.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ReceiptCalculationComponent } from "./receipt-calculation/receipt-calculation.component";
+
+import { MatButtonModule } from "@angular/material/button";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ReceiptCalculationComponent
-  ],
+  declarations: [AppComponent, ReceiptCalculationComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    NoopAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
