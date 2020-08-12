975975eaf646a7afab469c8e159e5452e465f087
Jackseed <vinkho.c@gmail.com>
2020-08-12 10:25:11 +0200

    style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ReceiptCalculationComponent } from "./receipt-calculation/receipt-calculation.component";
 
+import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
@@ -15,6 +16,7 @@ import { NoopAnimationsModule } from "@angular/platform-browser/animations";
     AppRoutingModule,
     MatButtonModule,
     NoopAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
